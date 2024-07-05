8256030770bcd412c30e28ce0c6a6cd9c92c53e4
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2024-07-05 17:49:27 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log('Script Testing 1')
+document.getElementById('games').addEventListener('click', function () {
+    window.location.href = "https://www.google.com"
+});
+document.getElementById('games1').addEventListener('click', function () {
+    window.location.href = "https://www.google.com"
+});
+document.getElementById('games2').addEventListener('click', function () {
+    window.location.href = "https://www.google.com"
+});
+document.getElementById('games3').addEventListener('click', function () {
+    window.location.href = "https://www.youtube.com"
+});
+document.getElementById('games4').addEventListener('click', function () {
+    window.location.href = "https://endgamingwithharsh.netlify.app"
+});
